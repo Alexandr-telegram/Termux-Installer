3394df5c01b1e7ae92ba0112b6bc4ef54b006fe1
Alexandr-telegram <sergey.bratkeviich@gmail.com>
2021-01-21 17:54:47 +0400

    Первая версия

---
+++ hackone.sh
@@ -0,0 +1,78 @@
+echo     ▀█▀ █▀▀ █▀▄ ██▄██ █░░█ █░█
+echo     ░█░ █▀▀ █▀▄ █░▀░█ █░░█ ▄▀▄
+echo     ░▀░ ▀▀▀ ▀░▀ ▀░░░▀ ░▀▀░ ▀░▀ 
+
+echo    ▀█▀ █▄░█ ▄▀▀ ▀█▀ ▄▀▄ █░░ █░░ █▀▀ █▀▄
+echo    ░█░ █▀██ ░▀▄ ░█░ █▄█ █░░ █░░ █▀▀ █▀▄
+echo    ▀▀▀ ▀░░▀ ▀▀░ ░▀░ ▀░▀ ▀▀▀ ▀▀▀ ▀▀▀ ▀░▀ 
+
+echo                             by Alexandr O
+
+apt-get install curl -y
+
+pkg install wget -y
+
+pkg install git -y
+
+pkg install python -y
+
+pkg install python 2 -y
+
+pkg install php -y
+
+pkg install open-ssh -y
+
+pkg install html -y
+
+pkg install hydra -y
+
+pkg install nano -y
+
+echo Все нужные пакеты установлены!Начинается установка metasploit!
+
+wget http://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh
+
+chmod +x metasploit.sh
+
+echo Metasploit установлен! Для запуска метасплоита введите msfconsole после окончания установки!
+
+git clone https://github.com/thewhiteh4t/seeker
+
+git clone https://github.com/xd20111/saycheese
+
+git clone https://github.com/greyli/sayhello
+
+git clone https://github.com/Mebus/cupp
+
+git clone https://github.com/evait-security/weeman
+
+cd weeman chmod +x weeman.py 
+
+cd ..
+
+git clone https://github.com/sqlmapproject/sqlmap
+
+git clone https://github.com/649/Memcrashed-DDoS-Exploit/
+
+git clone https://github.com/pentestxr/Fludilka
+
+git clone https://github.com/LimerBoy/Impulse
+
+git clone https://github.com/Denishnc/b0mb3r
+
+git clone https://github.com/duyet/bruteforce-database
+
+echo Пакет программ из github установлен!
+
+cd storage
+
+mkdir WI-FI_Bruteforce
+
+cd WI-FI_Bruteforce
+
+git clone https://github.com/faizann24/wifi-bruteforcer-fsecurify
+
+echo Программа для брутфорса паролей , установлена!
+echo Все программы были скачаны!
+clear
+echo Спасибо за использование!
